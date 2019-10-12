0ab9e599f5df721ffc9b2d2f1598e3da0bd683b0
unknown <aris.ppd@gmail.com>
2019-10-12 21:19:43 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+function weightedRand(spec) {
+  var i, j, table=[];
+  for (i in spec) {
+    // The constant 10 below should be computed based on the
+    // weights in the spec for a correct and optimal table size.
+    // E.g. the spec {0:0.999, 1:0.001} will break this impl.
+    for (j=0; j<spec[i]*10; j++) {
+      table.push(i);
+    }
+  }
+  return function() {
+    return table[Math.floor(Math.random() * table.length)];
+  }
+}
+
+var rand012 = weightedRand({0:0.8, 1:0.1, 2:0.1});
+rand012(); // random in distribution...
